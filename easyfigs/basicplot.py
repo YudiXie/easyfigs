cde812eb26aa33c15e9405fec79c49fb80edd572
YudiXie <yudixie235@gmail.com>
2024-08-23 11:55:38 -0400

    update bar plot

--- easyfigs/basicplot.py
+++ easyfigs/basicplot.py
@@ -60,56 +60,40 @@ def add_filled_lines(ax, data_dict, **kwargs):
                             alpha=0.2, linewidth=0.0, **fl_kwargs)
 
 
-def bar_groups(ax,
-               x_axis_labels,
-               data_dict,
-               ylim=None,
-               x_label=None,
-               y_label=None,
-               fig_title=None,
-               bar_label=True,
-               legend_title=None,
-               legend_fontsize=10,
-               ):
+def bar_groups(ax, x_axis_labels, data_dict, bar_label=True):
     """
     make a bar plot of multiple groups of data
 
     Args:
         data_dict: dict, the data to plot,
             each key is the group label of a set of points
-            each value is a dict with keys 'x', 'y', 'error'
-            they are list or array of the same length
-            'x' is the x axis data (optional)
-            'y' is the y axis data (same length as x_axis_labels)
-            'error', optional, is the error bar data
-            'color', optional, is the color of the points
+            each value is a dict with keys 'y', 'error'
+            'x' is not used
+            'y' is the y axis data, shape (len(x_axis_labels), )
+            'error', optional, is the error bar data, shape (len(x_axis_labels), )
+            'kwargs', optional, additional keyword arguments for bar
+    Returns:
+        x_axis: array, the x axis data
+        rect: list of Rectangles, the bars
     """
     num_groups = len(data_dict.keys())
     width = 0.7 / num_groups  # the width of the bars
     x_axis = np.arange(len(x_axis_labels))  # the label locations
 
+    rect_list = []
     for (i, (key, value)) in enumerate(data_dict.items()):
         assert len(value['y']) == len(x_axis_labels)
         offset = i * width - ((num_groups - 1) * width / 2)
-        kwargs = {'label': key, 'capsize': width*15, 'ecolor': 'black', 'alpha': 0.5}
+        kwargs = {'label': key, 'capsize': width * 15, 'ecolor': 'black'}
         if 'error' in value:
             kwargs['yerr'] = value['error']
-        if 'color' in value:
-            kwargs['color'] = value['color']
+        if 'kwargs' in value:
+            kwargs.update(value['kwargs'])
         rect = ax.bar(x_axis + offset, value['y'], width, **kwargs)
+        rect_list.append(rect)
         if bar_label:
             ax.bar_label(rect, padding=3, fmt='%.2f')
-
+    
     ax.set_xticks(x_axis, x_axis_labels)
-
-    legend_kwargs = {'title': legend_title, 'fontsize': legend_fontsize}
-    ax.legend(**legend_kwargs)
-
-    axis_kwargs = {'ylim': ylim,
-                   'xlabel': x_label,
-                   'ylabel': y_label,
-                   'title': fig_title,
-                   }
-    ax.set(**axis_kwargs)
-    remove_top_right_spines(ax)
-    return rect
+    ax.legend()
+    return x_axis, rect_list
